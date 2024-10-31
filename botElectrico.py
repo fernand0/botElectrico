e42bf6d348f7d3f7b49df32f1a7811e868fa2021
fernand0 <fernand0@elmundoesimperfecto.com>
2024-10-31 21:51:09 +0100

    2024-10-31

--- botElectrico.py
+++ botElectrico.py
@@ -447,7 +447,6 @@ def main():
                                                                 delta,
                                                                 dataNext)
         graficaDiaPlot(dataNext)
-        return
         table = makeTable(values, minDay, maxDay)
         js = makeJs(values, minDay, maxDay, str(nowNext).split(' ')[0])
         with open(f"/tmp/kk.js", 'w') as f:
